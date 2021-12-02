e1e708afbd91749592bd0490040dc01a83abae23
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-02 00:36:41 -0500

    Day 02 part 1

---
+++ Day 02/Program.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode;
+using AdventOfCode.Parsing;
+using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Day_02 {
+    internal class Program : ProgramStructure<IEnumerable<SubInstruction>> {
+
+        Program() : base(new Parser()
+            .Filter(new LineReader())
+            .Filter(x => x.Split())
+            .FilterCreate<SubInstruction>()
+        ) { }
+
+        static void Main(string[] args) {
+            new Program().Run(args);
+        }
+
+        protected override object SolvePart1(IEnumerable<SubInstruction> input) {
+            Point position = new();
+
+            foreach(SubInstruction instruction in input) {
+                position += instruction;
+            }
+
+            return position.X * position.Y;
+        }
+
+        protected override object SolvePart2(IEnumerable<SubInstruction> input) {
+            return null;
+        }
+
+    }
+}
---
+++ Day 02/SubInstruction.cs
@@ -0,0 +1,66 @@
+﻿using AdventOfCode.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02 {
+    public enum SubDirection {
+        forward,
+        down,
+        up
+    }
+
+    public struct SubInstruction : IObjectParser<string[]> {
+        public SubDirection Direction;
+        public int Units;
+
+        public int dx {
+            get {
+                if(Direction == SubDirection.forward) {
+                    return Units;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        public int dy {
+            get {
+                if (Direction == SubDirection.up) {
+                    return -Units;
+                } else if (Direction == SubDirection.down) {
+                    return Units;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        public SubInstruction(SubDirection direction, int units) {
+            Direction = direction;
+            Units = units;
+        }
+
+        public void Parse(string[] input) {
+            Direction = Enum.Parse<SubDirection>(input[0]);
+            Units = int.Parse(input[1]);
+        }
+
+        public static Point operator +(Point pos, SubInstruction instruction) {
+            return new Point(
+                pos.X + instruction.dx,
+                pos.Y + instruction.dy
+            );
+        }
+
+        public static Point operator -(Point pos, SubInstruction instruction) {
+            return new Point(
+                pos.X - instruction.dx,
+                pos.Y - instruction.dy
+            );
+        }
+    }
+}
