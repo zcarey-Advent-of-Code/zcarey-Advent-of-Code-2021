c6e6c6e998c25a5c02c9b16c61bd851936dacd1a
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-09 15:56:21 -0500

    Day 7 part 2

--- Day 07/Program.cs
+++ Day 07/Program.cs
@@ -34,7 +34,21 @@ namespace Day_07 {
         }
 
         protected override object SolvePart2(int[] input) {
-            return null;
+            int average = input.Sum() / input.Length;
+            // Since this is a discrete problem, we need to round depending on the number of crabs....
+            // But since this is programming and I am lazy, we are just going to take the smaller of average and average + 1
+
+            Func<int, long> calculateFuel = (int target) => { return input.Select(x => CalculateComplexFuel(Math.Abs(x - target))).Sum(); };
+            long floorFuel = calculateFuel(average);
+            long ceilFuel = calculateFuel(average + 1);
+
+            return Math.Min(floorFuel, ceilFuel);
+        }
+
+        private static long CalculateComplexFuel(long distance) {
+            // Calculate 1 + 2 + 3 + 4 + ... + distance
+            // y = 0.5x^{2}+0.5x+0
+            return (distance * (distance + 1)) / 2;
         }
 
     }
