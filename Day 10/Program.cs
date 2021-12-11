cd12e02255757017465060395a077bdfe4056512
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-11 16:15:09 -0500

    Day 10 part 2

--- Day 10/Program.cs
+++ Day 10/Program.cs
@@ -25,11 +25,18 @@ namespace Day_10 {
             { '<', '>' }
         };
 
-        private static Dictionary<char, int> ScoreTable = new() {
+        private static Dictionary<char, int> SyntaxScoreTable = new() {
             { ')', 3 },
             { ']', 57},
             { '}', 1197},
             { '>', 25137 }
+        };
+
+        private static Dictionary<char, int> AutocompleteScoreTable = new() {
+            { ')', 1 },
+            { ']', 2 },
+            { '}', 3 },
+            { '>', 4 }
         };
 
         protected override object SolvePart1(string[] lines) {
@@ -46,7 +53,7 @@ namespace Day_10 {
                         char closingBracket = ClosingBrackets[chunks.Pop()];
                         if(c != closingBracket) {
                             // Corrupted line!
-                            errorScore += ScoreTable[c];
+                            errorScore += SyntaxScoreTable[c];
                             break;
                         }
                     }
@@ -57,7 +64,39 @@ namespace Day_10 {
         }
 
         protected override object SolvePart2(string[] lines) {
-            return null;
+            List<long> scores = new();
+
+            foreach (string line in lines) {
+                Stack<char> chunks = new Stack<char>();
+                bool corruped = false;
+                foreach (char c in line) {
+                    if (ClosingBrackets.ContainsKey(c)) {
+                        // Starting a new chunk!
+                        chunks.Push(c);
+                    } else {
+                        // We are expecting the correct closing bracket
+                        char closingBracket = ClosingBrackets[chunks.Pop()];
+                        if (c != closingBracket) {
+                            // Corrupted line!
+                            corruped = true;
+                            break;
+                        }
+                    }
+                }
+
+                // End of line reached, get the required closing brackets to autocomplete the line
+                if (!corruped) {
+                    long score = 0;
+                    while (chunks.Count > 0) {
+                        char closingBracket = ClosingBrackets[chunks.Pop()];
+                        score = score * 5 + AutocompleteScoreTable[closingBracket];
+                    }
+                    scores.Add(score);
+                }
+            }
+
+            scores.Sort();
+            return scores[scores.Count / 2];
         }
 
     }
