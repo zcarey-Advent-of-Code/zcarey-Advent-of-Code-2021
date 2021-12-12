f5f6fe88630d5556e34c9b813a91ccad7b943908
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-12 00:40:44 -0500

    Day 12 part 1

---
+++ Day 12/Program.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode;
+using AdventOfCode.Parsing;
+using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_12 {
+    internal class Program : ProgramStructure<Graph> {
+
+        Program() : base(new Parser()
+            .Filter(new LineReader())
+            .Filter(new SeparatedParser("-"))
+            .ForEach(
+                new Parser<IEnumerable<string>>()
+                .ToArray()
+                .Parse(x => new Tuple<string, string>(x[0], x[1]))
+            )
+            .Create<Graph>()
+        ) { }
+
+        static void Main(string[] args) {
+            new Program().Run(args);
+            //new Program().Run(args, "Example.txt");
+        }
+
+        protected override object SolvePart1(Graph input) {
+            return FindPaths(input.StartNode, input.EndNode);
+        }
+
+        protected override object SolvePart2(Graph input) {
+            return null;
+        }
+
+        public int FindPaths(Cave searchCave, Cave targetCave) {
+            if (searchCave == targetCave) return 1; // We found a path!
+
+            int totalPaths = 0;
+            searchCave.Traversed = true;
+            {
+                foreach (Cave cave in searchCave.Connections) {
+                    if (!cave.Traversed) {
+                        totalPaths += FindPaths(cave, targetCave);
+                    }
+                }
+            }
+            searchCave.Traversed = false;
+
+            return totalPaths;
+        }
+
+    }
+}
