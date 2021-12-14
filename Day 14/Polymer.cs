cd8cad5c3a89be46e3c1e453b7346a72d977304f
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-14 08:47:12 -0500

    Day 14 part 1

---
+++ Day 14/Polymer.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14 {
+    public class Polymer : IObjectParser<string> {
+
+        public LinkedList<PolymerElement> Elements = new();
+
+        public void Parse(string input) {
+            foreach(char c in input) {
+                Elements.AddLast(new PolymerElement(c));
+            }
+        }
+    }
+
+    public class PolymerElement {
+
+        public char Element { get; set; }
+        public readonly List<char> AddBuffer = new();
+
+        public PolymerElement() { }
+        public PolymerElement(char element) {
+            Element = element;
+        }
+
+    }
+}
