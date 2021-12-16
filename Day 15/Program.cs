dce7e0f990866a2e9ef6cd64f08a889d7a460295
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-12-16 00:48:19 -0500

    Day 15 part 2

--- Day 15/Program.cs
+++ Day 15/Program.cs
@@ -27,15 +27,25 @@ namespace Day_15 {
         }
 
         protected override object SolvePart1(int[][] input) {
-            Point target = new Point(input[input.Length - 1].Length - 1, input.Length - 1);
-            IEnumerable<Point> path = FindSafestPath(input, new Point(0, 0), target);
+            int y = input.Length;
+            int x = input[y - 1].Length;
+            Point target = new(x - 1, y - 1);
+
+            IEnumerable<Point> path = FindSafestPath(input, new Point(0, 0), target, false);
             // Skip the start position danger
-            int danger = path.Skip(1).Select(p => input[p.Y][p.X]).Sum();
+            int danger = path.Skip(1).Select(p => GetDangerLevel(input, p, false)).Sum();
             return danger;
         }
 
         protected override object SolvePart2(int[][] input) {
-            return null;
+            int y = input.Length;
+            int x = input[y - 1].Length;
+            Point target = new(x * 5 - 1, y * 5 - 1);
+            
+            IEnumerable<Point> path = FindSafestPath(input, new Point(0, 0), target, true);
+            // Skip the start position danger
+            int danger = path.Skip(1).Select(p => GetDangerLevel(input, p, true)).Sum();
+            return danger;
         }
 
         private static readonly Size[] Directions = new Size[] { 
@@ -46,7 +56,7 @@ namespace Day_15 {
             new Size(-1, 0)
         };
 
-        private static IEnumerable<Point> FindSafestPath(int[][] input, Point start, Point target) {
+        private static IEnumerable<Point> FindSafestPath(int[][] input, Point start, Point target, bool adjustCave) {
             // As always, my favorite website for pathing (dijkstra):
             // https://www.redblobgames.com/pathfinding/a-star/introduction.html#dijkstra
             PriorityQueue<Point, int> frontier = new();
@@ -65,12 +75,13 @@ namespace Day_15 {
 
                 foreach(Size direction in Directions) {
                     Point next = current + direction;
-                    if(next.X < 0 || next.Y < 0 || next.Y >= input.Length || next.X >= input[next.Y].Length) {
+                    int danger = GetDangerLevel(input, next, adjustCave);
+                    if(danger < 0) { 
                         // Invalid position
                         continue;
                     }
 
-                    int newCost = costSoFar[current] + input[next.Y][next.X];
+                    int newCost = costSoFar[current] + danger;
                     if (!costSoFar.ContainsKey(next) || newCost < costSoFar[next]) {
                         costSoFar[next] = newCost;
                         int priority = newCost; // Semantics, am I right?
@@ -93,6 +104,35 @@ namespace Day_15 {
             // Flip the list and return the path
             for(int i = reversePath.Count - 1; i >= 0; i--) {
                 yield return reversePath[i];
+            }
+        }
+
+        // Returns -1 if not a valid position
+        private static int GetDangerLevel(int[][] input, Point pos, bool adjustCave) {
+            if (!adjustCave) {
+                // Just do regular checks
+                if (pos.X < 0 || pos.Y < 0 || pos.Y >= input.Length || pos.X >= input[pos.Y].Length) {
+                    return -1;
+                }
+
+                return input[pos.Y][pos.X];
+            } else {
+                // This cave gets funky
+                int limitY = input.Length * 5;
+                if (pos.Y < 0 || pos.Y >= limitY) return -1;
+                int y = pos.Y % input.Length;
+
+                int limitX = input[y].Length * 5;
+                if (pos.X < 0 || pos.X >= limitX) return -1;
+                int x = pos.X % input[y].Length;
+
+                // Calculate danger adjustment
+                int chunkX = pos.X / input[y].Length;
+                int chunkY = pos.Y / input.Length;
+                int dangerAdjustment = chunkX + chunkY;
+
+                int danger = input[y][x] + dangerAdjustment;
+                return ((danger - 1) % 9) + 1; // The minus one and plus one is to deal with the wrap around, since mod wraps back to 0
             }
         }
 
