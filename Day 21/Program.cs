237bd6b30cf297f1e637c76733ab156cc58cd013
Zachary Carey <zach.carey@outlook.com>
2021-12-31 12:28:05 -0500

    cleaned up part 1

--- Day 21/Program.cs
+++ Day 21/Program.cs
@@ -28,44 +28,40 @@ namespace Day_21 {
             int pos2 = input[1];
             int score2 = 0;
 
-            Dice dice = new DeterministicDice();
-            return PlayGame(dice, ref pos1, ref score1, ref pos2, ref score2) * dice.Rolls;
+            int dice = -1;
+            int rolls = 0;
+
+            while (true) {
+                // On each player's turn, the player rolls the die three times and adds up the results.
+                int move1 = Roll(ref dice) + Roll(ref dice) + Roll(ref dice);
+                rolls += 3;
+                // Then, the player moves their pawn that many times forward around the track
+                pos1 = (pos1 + move1) % 10;
+                // After each player moves, they increase their score by the value of the space their pawn stopped on.
+                // NOTE: our board is 0-9, so we add one to get the actual board's 1-10
+                score1 += (pos1 + 1);
+                if (score1 >= 1000) {
+                    return score2 * rolls;
+                }
+
+                // Do the same for player 2
+                int move2 = Roll(ref dice) + Roll(ref dice) + Roll(ref dice);
+                rolls += 3;
+                pos2 = (pos2 + move2) % 10;
+                score2 += (pos2 + 1);
+                if (score2 >= 1000) {
+                    return score1 * rolls;
+                }
+            }
         }
 
         protected override object SolvePart2(int[] input) {
             return null;
         }
 
-        // Returns the loser's score
-        private static int PlayGame(Dice dice, ref int pos1, ref int score1, ref int pos2, ref int score2) {
-            while (Simulate(dice, ref pos1, ref score1, ref pos2, ref score2)) {
-            }
-
-            if(score1 >= 1000) {
-                return score2;
-            } else {
-                return score1;
-            }
-        }
-
-        // Returns true of the game should continue.
-        private static bool Simulate(Dice dice, ref int pos1, ref int score1, ref int pos2, ref int score2) {
-            // On each player's turn, the player rolls the die three times and adds up the results.
-            int move1 = dice.Roll() + dice.Roll() + dice.Roll();
-            // Then, the player moves their pawn that many times forward around the track
-            pos1 = (pos1 + move1) % 10;
-            // After each player moves, they increase their score by the value of the space their pawn stopped on.
-            // NOTE: our board is 0-9, so we add one to get the actual board's 1-10
-            score1 += (pos1 + 1);
-            if (score1 >= 1000) return false;
-
-            // Do the same for player 2
-            int move2 = dice.Roll() + dice.Roll() + dice.Roll();
-            pos2 = (pos2 + move2) % 10;
-            score2 += (pos2 + 1);
-            if (score2 >= 1000) return false;
-
-            return true;
+        private static int Roll(ref int dice) {
+            dice = (dice + 1) % 100;
+            return dice + 1; // The variable goes from 0-99, but the dice rolls from 1-100
         }
 
     }
